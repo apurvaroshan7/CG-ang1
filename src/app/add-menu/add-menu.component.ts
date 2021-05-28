083657323b01a1440a9401f1ab4a275a7b236563
apurvaroshan7 <84707289+apurvaroshan7@users.noreply.github.com>
2021-05-28 14:31:28 +0530

    Update add-menu.component.ts

--- src/app/add-menu/add-menu.component.ts
+++ src/app/add-menu/add-menu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Menu } from '../Menu';
+import { Menu } from '../menu';
 import { MenuService } from '../menu.service';
 
 @Component({
