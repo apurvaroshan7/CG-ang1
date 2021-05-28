3ccbb283aae0e95894d50840d73ff482865c2fdb
apurvaroshan7 <84707289+apurvaroshan7@users.noreply.github.com>
2021-05-28 14:31:49 +0530

    Update edit-menu.component.ts

--- src/app/edit-menu/edit-menu.component.ts
+++ src/app/edit-menu/edit-menu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Menu } from '../Menu';
+import { Menu } from '../menu';
 import { MenuService } from '../menu.service';
 
 @Component({
